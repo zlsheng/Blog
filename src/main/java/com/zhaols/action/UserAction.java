696862c6d5e28556f8aa348f9bd2498286518a2e
zhaols <1219466321@qq.com>
2018-08-19 15:24:44 +0800

    struts2 动态调用待配置

--- src/main/java/com/zhaols/action/UserAction.java
+++ src/main/java/com/zhaols/action/UserAction.java
@@ -1,7 +1,7 @@
 package com.zhaols.action;
 
 public class UserAction {
-    public String test(){
+    public String test123(){
         System.out.println("test方法");
         System.out.println("Struts2 配置成功");
         return "success";
