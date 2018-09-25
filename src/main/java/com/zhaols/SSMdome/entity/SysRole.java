3291cedbba01dc92177eb9a460808397970f9a08
zhaols <1219466321@qq.com>
2018-09-25 21:19:31 +0800

    细节实现

---
+++ src/main/java/com/zhaols/SSMdome/entity/SysRole.java
@@ -0,0 +1,140 @@
+package com.zhaols.SSMdome.entity;
+
+import com.zhaols.SSMdome.BasicClassDri.BasicEntity;
+
+import java.util.Date;
+
+public class SysRole extends BasicEntity<String> {
+
+  private String id;
+  private String code;
+  private String name;
+  private String remark;
+  private String description;
+  private String updator;
+  private String updatorName;
+  private Date updateTime;
+  private String creator;
+  private String creatorName;
+  private Date createTime;
+  private String status;
+  private String isDel;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getRemark() {
+    return remark;
+  }
+
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
+
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+
+  public String getUpdator() {
+    return updator;
+  }
+
+  public void setUpdator(String updator) {
+    this.updator = updator;
+  }
+
+
+  public String getUpdatorName() {
+    return updatorName;
+  }
+
+  public void setUpdatorName(String updatorName) {
+    this.updatorName = updatorName;
+  }
+
+
+  public Date getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
+
+
+  public String getCreator() {
+    return creator;
+  }
+
+  public void setCreator(String creator) {
+    this.creator = creator;
+  }
+
+
+  public String getCreatorName() {
+    return creatorName;
+  }
+
+  public void setCreatorName(String creatorName) {
+    this.creatorName = creatorName;
+  }
+
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+
+  public String getIsDel() {
+    return isDel;
+  }
+
+  public void setIsDel(String isDel) {
+    this.isDel = isDel;
+  }
+
+}
--- src/main/java/com/zhaols/SSMdome/service/Impl/SysResourcesService.java
+++ src/main/java/com/zhaols/SSMdome/service/Impl/SysResourcesService.java
@@ -6,6 +6,7 @@ import com.zhaols.SSMdome.entity.SysResources;
 import com.zhaols.SSMdome.mapper.SysResourcesMapper;
 import com.zhaols.SSMdome.service.ISysResourcesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author zhaols
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Description: TODO: 添加功能描述
  * @date 2018-09-25 16:49
  */
+@Service
 public class SysResourcesService extends SuperService<SysResources> implements ISysResourcesService {
     @Autowired
     private SysResourcesMapper resourcesMapper;
